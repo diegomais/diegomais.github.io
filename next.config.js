f6b90a81a8c76c1d33de61e5a8ed9cfb12a8b18f
Diego Mais <diegomais.pro@gmail.com>
2024-01-20 11:49:04 -0300

    build: update nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
